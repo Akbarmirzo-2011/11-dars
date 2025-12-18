e9a5f700c295ebcc159d0864fa6ad57e6a5e2f8b
Akbarmirzo-2011 <mehribonartiqova955@gmail.com>
2025-12-18 22:01:00 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-const buttons = document.querySelectorAll("button");
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
 
-const resultEl = document.getElementById("result");
+const today = new Date();
+ 
+const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
 
-const playerScoreEl = document.getElementById("user-score");
+const allMonth = ["January","Fabuary","March","April","May","June","July","August","September","October","November","December",];
 
-const computerScoreEl = document.getElementById("computer-score");
-
-let playerScore = 0;
-let computerScore = 0;
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const result = playRound(button.id, computerPlay());
-    resultEl.textContent = result;
-    
-  });
-});
-
-function computerPlay() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomChoice];
-}
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    playerScore++;
-    playerScoreEl.textContent = playerScore;
-    return "You win! " + playerSelection + " beats " + computerSelection;
-  } else {
-    computerScore++;
-    computerScoreEl.textContent = computerScore;
-    return "You lose! " + computerSelection + " beats " + playerSelection;
-  }
-}
+date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonth[today.getMonth()];
+year.innerHTML = today.getFullYear();
